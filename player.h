b8889d8b5ab1378d05376b1e38bfa62ca04bce72
romanpltk <92595164+romanpltk@users.noreply.github.com>
2022-06-17 19:33:36 +0300

    Add files via upload

---
+++ player.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+#include "settings.h"
+#include <string>
+class Player {
+private:
+	sf::Texture texture;
+	
+	sf::Sprite sprite;
+	sf::Clock clock1;
+	sf::Clock clock2;
+	float speed = 0.f;
+	static std::string player_anim_files[];
+	static std::string player_jump_files[];
+	size_t frame = 0;
+	size_t frames = 0;
+	size_t frame_delay = 55;
+	bool isJump = false;
+	int hp = PLAYER_HP;
+	int distance = 0;
+	
+public:
+	Player();
+	void update();
+	void draw(sf::RenderWindow& window);
+	sf::FloatRect getHitBox();
+	sf::Vector2f getPosition();
+	void decreaseHp(size_t);
+	int getHP();
+	int Distance();
+};
